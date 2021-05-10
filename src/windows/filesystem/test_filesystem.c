901846b58bbe683a3d699efe4ab0be9e47a5cde3
BatteryShark <batteryshark@outlook.com>
2021-05-10 01:17:38 -0400

    updated test for pdxfs

--- src/windows/filesystem/test_filesystem.c
+++ src/windows/filesystem/test_filesystem.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 //#include "filesystem/fs_utils_win.h"
-#include "filesystem/fs_redirect.h"
+#include "../../shared/fs_redirect.h"
 
 int main(int argc, char *argv[]){
     //printf("Path Exists: %d\n",path_exists(L"\\??\\C:\\VX"));
@@ -15,9 +15,11 @@ int main(int argc, char *argv[]){
  //   resolve_abspath(L"\\??\\D:\\vxsymlink.ini",out_path,1);
   //  wprintf(L"out path: %ls\n",out_path);
         
-    putenv("PDXFS_ROOT=c:\\tmp\\vx\\30196360\\vxmap");
+    putenv("PDXFS_ROOT=C:\\vxtmp\\CPB-WV7-UJ4\\map");
     char* redirected_path = NULL;
-    int res = fs_redirect(argv[1],0, 1,1, 0, 0, &redirected_path);
+    char* inst = "\\??\\c:\\vxtmp\\cpb-wv7-uj4\\map\\c\\programdata\\nvidia corporation\\nv_cache\\35c5bbd7ddb5aa7de69f6e9399aa4605_fce8395c8fd8a9cb_387ce0633917037e_0_0.0.toc";
+    
+    int res = fs_redirect(inst,0, 1,0, 0, 0, &redirected_path);
     if(res){
         printf("Redirected: %d %s\n",res,redirected_path);
     }else{
